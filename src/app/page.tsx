e473b0991207d8f0fb59334f0af631654d737cf0
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-28 19:30:16 +0530

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,8 +24,7 @@ export default function Home() {
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          
+        >  
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
             Explore starter templates for Next.js.
           </p>
