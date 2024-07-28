92266c11884552422a52bc0b4a993a7d64ecf285
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-28 19:30:02 +0530

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,12 +27,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
+          
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
             Explore starter templates for Next.js.
           </p>
