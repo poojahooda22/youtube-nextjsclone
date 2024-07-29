3ad003cd6364e0fabcd0d818a063ff24ed418035
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-29 19:14:20 +0530

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="py-[4vw]">
+    <main className="py-[1vw]">
       <VideoGrid />
     </main>
   );
