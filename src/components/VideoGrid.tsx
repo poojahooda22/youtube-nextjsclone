09d20ddd1e7b33811e0906005b915decb3fdee48
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-29 19:44:30 +0530

    updated video grid

--- src/components/VideoGrid.tsx
+++ src/components/VideoGrid.tsx
@@ -38,7 +38,7 @@ const data = [
 
 
 export const VideoGrid = () => {
-    return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+    return <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
         {data.map((video, index) => {
             return (
                 <Card 
