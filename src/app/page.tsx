70bb56424543da10fe45c51271eb569591751fed
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-29 18:46:03 +0530

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Card from "@/components/Card";
+import { VideoGrid } from "@/components/VideoGrid";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="py-[4vw]">
-      <Card />
+      <VideoGrid />
     </main>
   );
 }
