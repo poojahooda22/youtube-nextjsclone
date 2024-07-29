64c9b2e91551ee9f4388977237a8dd069a5c1412
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-29 18:46:10 +0530

    updated card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,43 +2,45 @@ import React from 'react'
 import { BiDotsVerticalRounded } from "react-icons/bi";
 
 
-const Card = () => {
+const Card = (props: any) => {
   return (
     <div className=''>
-        <div className='w-full sm:w-[50vw] h-[60vw] '>
-            <div className='w-full h-[42vw]'>
+        <div className='w-full sm:w-[30vw] h-[60vw] '>
+            <div className='w-full h-[42vw] sm:w-[30vw] sm:h-[20vw]'>
                 <img 
-                    src="https://img.freepik.com/free-psd/3d-rendering-gamer-room-illustration_23-2151642704.jpg?uid=R3651325&ga=GA1.1.678771452.1700385267" 
+                    src={props.image} 
                     alt="placeholder"
-                    className='w-full h-full object-cover sm:rounded-lg' 
+                    className='w-full h-full object-cover sm:rounded-xl' 
                 />
             </div>
-            <div className='w-full flex justify-between mt-[4vw] px-[4vw]'>
-                <div className='flex gap-[2.4vw]'>
+            <div className='w-full flex justify-between items-start mt-[4vw] sm:mt-[1vw] px-[4vw] sm:px-0'>
+                <div className='flex gap-[2.4vw] sm:gap-[1.2vw]'>
                     <img 
-                        src="https://img.freepik.com/free-photo/young-beautiful-woman-pink-warm-sweater-natural-look-smiling-portrait-isolated-long-hair_285396-896.jpg?uid=R3651325&ga=GA1.1.678771452.1700385267&semt=sph" 
+                        src={props.thumbImage} 
                         alt="placeholder" 
-                        className='w-10 h-10 object-cover rounded-full'
+                        className='w-10 h-10 sm:w-16 sm:h-16 object-cover rounded-full'
                     />
-                    <div className='w-2/3 '>
-                        <h1 className='text-[3vw]'>
-                            Paris Olympics 2024: Manu Bhaker wins historic bronze in women’s...
+                    <div className=' '>
+                        <h1 className='text-[3.6vw] sm:text-[1vw]'>
+                            {props.title}
                         </h1>
-                        <div>
-                            <h3 className='text-[1.5vw]'>
-                                The Hindu
-                            </h3>
-                            {/* //i*/}
-                        </div>
-                        <div className='flex gap-2 text-[1.5vw]'>
-                            <p>1.6K views</p>
-                            <p>25 minutes ago</p>
+                        <div className='flex gap-2 text-[2.4vw] sm:text-[.8vw] text-zinc-400'>
+                            <div>
+                                <h3 className=''>
+                                    {props.author}
+                                </h3>
+                            </div>
+                            <div className='flex gap-2 '>
+                                <p>{props.views}</p>
+                                <p>{props.timeStamp}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
                 
                 <BiDotsVerticalRounded 
-                    className='w-8 h-8 text-[#ffffff]'
+                    size={24}
+                    className='sm:h-[1.5vw] text-[#ffffff]'
                 />
             </div>
         </div>
