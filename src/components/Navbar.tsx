7e7a40bae89d6385e49932eabed0ba72d021536c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-30 10:35:16 +0530

    updated navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Searchbar from './Searchbar'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='flex items-center justify-between'>
+      <div>Youtube</div>
+      <div><Searchbar/></div>
+      <div>person</div>
     </div>
   )
 }
