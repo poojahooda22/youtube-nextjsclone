3ecd5eecb9c9c5d2b99fa679e5730f5769e97c45
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-30 10:35:01 +0530

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Card from "@/components/Card";
+import Navbar from "@/components/Navbar";
 import { VideoGrid } from "@/components/VideoGrid";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="py-[1vw]">
+      <Navbar />
       <VideoGrid />
     </main>
   );
