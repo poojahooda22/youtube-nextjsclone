1c66c23008963ea95617bf5cb89c76410a98e121
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-08 20:31:55 +0530

    card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,26 +5,29 @@ import { BiDotsVerticalRounded } from "react-icons/bi";
 const Card = (props: any) => {
   return (
 
-        <div className='w-full md:w-[30vw] lg:w-[40vw] p-2'>
-            <div className='w-full h-[42vw] sm:w-[24vw] sm:h-[16vw]'>
+        <div className='w-full md:w-[50vw] lg:w-[34vw] xl:w-[25vw] 2xl:w-[16vw] p-4'>
+            <div className='w-full h-[48vw] md:h-[22vw] lg:h-[19vw] xl:h-[15vw] 2xl:h-[9vw]'>
                 <img 
                     src={props.image} 
                     alt="placeholder"
-                    className='w-full h-full object-cover sm:rounded-xl' 
+                    className='w-full h-full object-cover rounded-xl' 
                 />
             </div>
-            <div className='w-full flex justify-between items-start mt-[4vw] sm:mt-[1vw] px-[4vw] sm:px-0'>
+            <div className='w-full flex justify-between items-start mt-[4vw] md:mt-[2vw] lg:mt-[1vw] xl:mt-[10px] px-[0vw] sm:px-0'>
                 <div className='flex gap-[2.4vw] sm:gap-[1.2vw]'>
                     <img 
                         src={props.thumbImage} 
                         alt="placeholder" 
-                        className='w-10 h-10 sm:w-16 sm:h-16 object-cover rounded-full'
+                        className='w-10 h-10 md:w-8 md:h-8 lg:w-10 lg:h-10 object-cover rounded-full'
                     />
-                    <div className=' '>
-                        <h1 className='text-[3.6vw] sm:text-[1vw]'>
+                    <div className='w-full'>
+                        <h1 className='text-[3.6vw] md:text-[1.9vw] lg:text-[1.5vw] xl:text-[1vw] 2xl:text-[.7vw] font-medium'>
                             {props.title}
                         </h1>
-                        <div className='flex gap-2 text-[2.5vw] sm:text-[.8vw] text-zinc-400'>
+                        <div 
+                            className='flex gap-2 md:gap-0 md:flex-col text-[2.5vw] md:text-[1.6vw] 
+                            lg:text-[1.4vw] xl:text-[1vw] 2xl:text-[.6vw] text-zinc-400 pt-[.5vw] md:pt-[1px] '
+                        >
                             <div>
                                 <h3 className=''>
                                     {props.author}
@@ -40,7 +43,7 @@ const Card = (props: any) => {
                 
                 <BiDotsVerticalRounded 
                     size={24}
-                    className='sm:h-[1.5vw] text-[#ffffff]'
+                    className='md:w-[3vw] lg:w-[2.5vw] xl:w-[2vw] text-[#ffffff]'
                 />
             </div>
         </div>
