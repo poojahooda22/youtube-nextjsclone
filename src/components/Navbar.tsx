7b6ae55896a4ac328892a9ff1a2b0fc744622813
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-08 21:03:22 +0530

    updated the size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 
       <div className='w-2/3 hidden sm:block'><Searchbar/></div>
       <div className='flex items-start gap-2'>
-        <RiVideoAddLine />
+        <RiVideoAddLine size={28} color='#777777' />
         <IoMdNotificationsOutline size={30} color='#777777' />
         <Avatar sx={{ width: 36, height: 36 }}>P</Avatar>
       </div>
