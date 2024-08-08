70c86fac0d6d40717a0d2543f50f1cbe79ea5efa
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-08 20:32:20 +0530

    updated video grid

--- src/components/VideoGrid.tsx
+++ src/components/VideoGrid.tsx
@@ -32,13 +32,29 @@ const data = [
         author: 'Nikhil Kamath',
         views: '543K views',
         timeStamp: '4 years ago'
+    },
+    {
+        title: "Ep.2 Trailer | People by WTF : Nikhil Kamath & Ranbir Kapoor",
+        image: 'pick1.webp',
+        thumbImage: 'nikhil.jpg',
+        author: 'Nikhil Kamath',
+        views: '543K views',
+        timeStamp: '4 years ago'
+    },
+    {
+        title: "Ep.2 Trailer | People by WTF : Nikhil Kamath & Ranbir Kapoor",
+        image: 'pick1.webp',
+        thumbImage: 'nikhil.jpg',
+        author: 'Nikhil Kamath',
+        views: '543K views',
+        timeStamp: '4 years ago'
     }
 
 ]
 
 
 export const VideoGrid = () => {
-    return <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+    return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6">
         {data.map((video, index) => {
             return (
                 <Card 
