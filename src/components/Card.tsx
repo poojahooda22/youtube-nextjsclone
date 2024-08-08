1b427416c14bdfb262c1f866c75517c8fb706b0f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-08 22:11:37 +0530

    added

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -32,7 +32,8 @@ const Card = (props: any) => {
                         <div 
                             className='flex gap-2 md:gap-0 md:flex-col 
                             text-[2.5vw] md:text-[1.6vw] 
-                            lg:text-[1.4vw] xl:text-[1vw] 2xl:text-[.6vw] text-zinc-400 pt-[.5vw] md:pt-[1px] '
+                            lg:text-[1.4vw] xl:text-[1vw] 2xl:text-[.6vw] 
+                            text-zinc-400 pt-[.5vw] md:pt-[1px] '
                         >
                             <div>
                                 <h3 className=''>
