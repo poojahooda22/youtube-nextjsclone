c0086b1d16ae9e2296a96cf95dbe075d325bd381
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-08 21:58:22 +0530

    added

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,7 +13,8 @@ const Card = (props: any) => {
                     className='w-full h-full object-cover rounded-xl' 
                 />
             </div>
-            <div className='w-full flex justify-between items-start mt-[4vw] md:mt-[2vw] lg:mt-[1vw] xl:mt-[10px] px-[0vw] sm:px-0'>
+            <div className='w-full flex justify-between items-start mt-[4vw] 
+            md:mt-[2vw] lg:mt-[1vw] xl:mt-[10px] px-[0vw] sm:px-0'>
                 <div className='flex gap-[2.4vw] sm:gap-[1.2vw]'>
                     <img 
                         src={props.thumbImage} 
