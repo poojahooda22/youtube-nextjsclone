26e171bd242c29bf8c735034148e831d2482f2f0
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-09 13:36:24 +0530

    updated navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,19 +9,19 @@ const Navbar = () => {
   return (
     <div className='flex items-start justify-between py-[1vw] px-[1vw]'>
       <div className=' flex items-start'>
-        <IoMenuSharp size={30} color='#999999' />  
+        <IoMenuSharp size={30} color='#999999' className='hidden sm:block' />  
         <img src="/ytlogo.svg" alt="logo" className='w-28 h-8' />  
       </div>
 
+      <div></div>
       <div className='w-2/3 hidden md:block'><Searchbar/></div>
-      <div className='flex items-start gap-4'>
+      <div className='sm:flex items-start gap-4 hidden'>
         <RiVideoAddLine size={28} color='#999999' />
         <div className='relative'>
           <IoMdNotificationsOutline size={30} color='#999999' />
           <div className='absolute top-0 right-[-.7vw] w-6 h-4 bg-red-600 
           rounded-full flex items-center justify-center text-[1.3vw]'>9+</div>
         </div>
-        
         <Avatar sx={{ width: 32, height: 32 }}>P</Avatar>
       </div>
     </div>
