91a3db62299ae8ded239f13aeaecb23dddcb117b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-09 19:57:26 +0530

    updated sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Sidebar = () => {
   return (
     <div className='w-[10vw] h-full bg-zinc-900'>
-        <div></div>
+        <div className=' flex items-start'>
+        <IoMenuSharp size={30} color='#999999' className='hidden sm:block' />  
+        <img src="/ytlogo.svg" alt="logo" className='w-28 h-8' />  
+      </div>
+        <div>Hello</div>
     </div>
   )
 }
