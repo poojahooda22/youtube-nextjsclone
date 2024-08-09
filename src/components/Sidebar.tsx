48c1eebfcf97b6961945f2562afcec7d612c4964
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-09 20:42:16 +0530

    updated sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,9 +3,9 @@ import { IoMenuSharp } from "react-icons/io5";
 
 const Sidebar = () => {
   return (
-    <div className='absolute top-0 w-[20vw] h-full bg-zinc-900'>
-        <div className=' flex items-start'>
-            <IoMenuSharp size={30} color='#999999' className='hidden sm:block' />  
+    <div className='absolute top-0 w-[20vw] h-full bg-zinc-900 py-[1.5vw] px-[1.5vw]'>
+        <div className='flex items-start'>
+            <IoMenuSharp size={28} color='#999999' className='hidden sm:block' />  
             <img src="/ytlogo.svg" alt="logo" className='w-28 h-8' />  
         </div>
         <div>Hello</div>
