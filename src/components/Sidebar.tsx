ec14b7f678ea40a2a4741d14ecd7827e6432de3a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-09 22:28:02 +0530

    updated sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -13,11 +13,11 @@ const Sidebar = () => {
             <img src="/ytlogo.svg" alt="logo" className='w-28 h-8' />  
         </div>
         <div className='flex items-center gap-4 mt-6 bg-zinc-700 py-1 px-2 rounded-lg'>
-            <GoHomeFill size={22} />
+            <GoHomeFill size={20} />
             <p className='text-[1.7vw]'>Home</p>
         </div>
         <div className='flex items-center gap-4 mt-2 px-2'>
-            <SiYoutubeshorts size={22} />
+            <SiYoutubeshorts size={20} />
             <p className='text-[1.7vw]'>Shorts</p>
         </div>
     </div>
