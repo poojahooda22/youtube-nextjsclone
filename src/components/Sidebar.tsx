24034464046c64779bc546b2cc459328cacd28f4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-09 22:35:00 +0530

    implementing the line

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -24,6 +24,7 @@ const Sidebar = () => {
             <MdOutlineSubscriptions size={20} />
             <p className='text-[1.5vw]'>Subscriptions</p>
         </div>
+        <div className=''></div>
     </div>
   )
 }
