acb0c27e8181d56391ca5246056f1fb9bd1e8caa
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-10 21:38:01 +0530

    updated search

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -6,7 +6,8 @@ const Searchbar = () => {
     <form className="max-w-lg mx-auto">
         <div className="flex">
             <div className="relative w-full">
-                <input type="search" id="search-dropdown" 
+                <input 
+                type="search" id="search-dropdown" 
                     className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 
                     rounded-full border-s-gray-50 border-s-2 
                     border border-gray-300 focus:ring-blue-500
