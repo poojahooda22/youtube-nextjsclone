38e56d2121318c0b5209913a141282f0603a4b47
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-10 20:51:24 +0530

    updated searchbar

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -29,7 +29,8 @@ const Searchbar = () => {
                         viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                     </svg>
-                    <span className="sr-only">Search</span>
+                    <span className="sr-only">
+                        Search</span>
                 </button>
             </div>
         </div>
