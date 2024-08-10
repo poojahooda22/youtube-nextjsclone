28f5920c2ba4853692ca1bd689871def1215a1f9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-10 20:36:26 +0530

    updated searchbar

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -18,7 +18,8 @@ const Searchbar = () => {
                     type="submit" 
                     className="absolute top-0 end-0 p-3.5 text-sm 
                     font-medium h-full text-white 
-                    rounded-e-full focus:ring-4 focus:outline-none focus:ring-blue-300"
+                    rounded-e-full focus:ring-4 
+                    focus:outline-none focus:ring-blue-300"
                 >
                     <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
