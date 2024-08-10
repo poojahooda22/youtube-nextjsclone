c945870843e935ecb2809086e47abed6a85a9c3a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-10 22:06:27 +0530

    updated

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -23,7 +23,7 @@ const Card = (props: any) => {
             <div className='flex gap-[2.4vw] sm:gap-[1.2vw]'>
                 <img 
                     src={props.thumbImage} 
-                        alt="placeholder" 
+                    alt="placeholder" 
                     className='w-10 h-10 md:w-8 md:h-8 lg:w-10 lg:h-10 
                     object-cover rounded-full'
                 />
