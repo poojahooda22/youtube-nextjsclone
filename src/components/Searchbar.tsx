bec6c6118d194f87d7812429010b5855cdb1b9bd
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-10 20:35:49 +0530

    updated searchbar

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -11,7 +11,8 @@ const Searchbar = () => {
                     rounded-full border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500
                     focus:border-blue-500 dark:bg-gray-900 dark:border-s-gray-700  dark:border-gray-600 
                     dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" 
-                    placeholder="Search" required 
+                    placeholder="Search" 
+                    required 
                 />
                 <button 
                     type="submit" 
