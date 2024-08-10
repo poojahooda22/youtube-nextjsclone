8fa96ce5b0f425664ea1ba390ae7ded006dcb4fd
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-10 21:37:18 +0530

    updated searchbar

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -8,7 +8,8 @@ const Searchbar = () => {
             <div className="relative w-full">
                 <input type="search" id="search-dropdown" 
                     className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 
-                    rounded-full border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500
+                    rounded-full border-s-gray-50 border-s-2 
+                    border border-gray-300 focus:ring-blue-500
                     focus:border-blue-500 dark:bg-gray-900 dark:border-s-gray-700  dark:border-gray-600 
                     dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" 
                     placeholder="Search" 
