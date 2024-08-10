3605b9478aa31e0e2ddfdb79bdb41ad459de6bf1
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-10 21:59:55 +0530

    added

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,7 +10,8 @@ const Card = (props: any) => {
                 <img 
                     src={props.image} 
                     alt="placeholder"
-                    className='w-full h-full object-cover rounded-xl' 
+                    className='w-full h-full object-cover 
+                    rounded-xl' 
                 />
             </div>
             <div 
