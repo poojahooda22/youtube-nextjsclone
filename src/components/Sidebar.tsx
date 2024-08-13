c7500900ec2f0ffc00b869d4ec256c43f05fd8fd
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-13 21:16:19 +0530

    updated sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -69,6 +69,10 @@ const Sidebar = () => {
             <img src="/ndtv.jpg" className='w-[2.4vw] h-[2.4vw] rounded-full' />
             <p className='text-[1.3vw]'>NDTV India</p>
         </div>
+        <div className='flex items-center gap-4 mt-3 px-2'>
+            <img src="/ndtv.jpg" className='w-[2.4vw] h-[2.4vw] rounded-full' />
+            <p className='text-[1.3vw]'>NDTV India</p>
+        </div>
 
     </div>
   )
