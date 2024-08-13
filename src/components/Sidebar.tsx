c47afd6a16e0326f668aa6bb71e7cc29602ca646
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-13 22:24:35 +0530

    added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -76,10 +76,12 @@ const Sidebar = () => {
             <HiOutlineStatusOnline size={20} color='red' />
         </div>
         <div className='flex items-center gap-4 mt-3 px-2'>
-            <div></div>
+            <div>
+                <img src="/express.jpg" className='w-[2.4vw] h-[2.4vw] rounded-full' />
+                <p className='text-[1.3vw]'>Indian Expr..</p>
+            </div>
 
-            <img src="/express.jpg" className='w-[2.4vw] h-[2.4vw] rounded-full' />
-            <p className='text-[1.3vw]'>Indian Expr..</p>
+            
             <HiOutlineStatusOnline size={20} color='red' />
         </div>
 
