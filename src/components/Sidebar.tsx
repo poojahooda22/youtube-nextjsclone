f04296f347ecc3808c68cd55832d1926d7244ebd
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-13 22:23:40 +0530

    updated

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -70,10 +70,10 @@ const Sidebar = () => {
         </div>
         <div className='flex items-center gap-4 mt-3 px-2'>
             <div>
-                
+                <img src="/nbc.jpg" className='w-[2.4vw] h-[2.4vw] rounded-full' />
+                <p className='text-[1.3vw]'>NBC News</p>
             </div>
-            <img src="/nbc.jpg" className='w-[2.4vw] h-[2.4vw] rounded-full' />
-            <p className='text-[1.3vw]'>NBC News</p>
+            
             <HiOutlineStatusOnline size={20} color='red' />
         </div>
         <div className='flex items-center gap-4 mt-3 px-2'>
