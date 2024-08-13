f7906357e9b2326918ca9d2f5d9cf0f7cf95b224
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-13 21:34:35 +0530

    updated p tag

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -67,7 +67,7 @@ const Sidebar = () => {
         </div>
         <div className='flex items-center gap-4 mt-3 px-2'>
             <img src="/ndtv.jpg" className='w-[2.4vw] h-[2.4vw] rounded-full' />
-            <p className='text-[1.3vw]'>NDTV India</p>
+            <p className='text-[1.3vw]'>NBC News</p>
         </div>
         <div className='flex items-center gap-4 mt-3 px-2'>
             <img src="/ndtv.jpg" className='w-[2.4vw] h-[2.4vw] rounded-full' />
