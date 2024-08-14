5d1f1ab66eb16079a9380e96c58a30f76a759311
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-14 14:27:56 +0530

    updated sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -34,8 +34,6 @@ const Sidebar = () => {
         <div className='w-full h-[1px] bg-zinc-700 mt-4 px-2'></div>
 
 
-
-
         <div className='flex items-center gap-4 mt-3 px-2'>
             <RiFolderVideoLine  size={20} />
             <p className='text-[1.3vw]'>Videos</p>
