b88a50a1f115a2b670d64b9d18c11536dbede60d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-14 13:37:23 +0530

    switched the comp

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -62,10 +62,7 @@ const Sidebar = () => {
         <h4 className='text-[1.5vw] mt-3 px-2'>Subscriptions</h4>
 
         <div className='flex items-center justify-between gap-4 mt-3 px-2'>
-            <div className='flex items-center gap-4'>
-                <img src="/ndtv.jpg" className='w-[2.4vw] h-[2.4vw] rounded-full' />
-                <p className='text-[1.3vw]'>NDTV India</p>
-            </div>        
+                   
             <HiOutlineStatusOnline size={20} color='red' />
         </div>
         <div className='flex items-center  justify-between  gap-4 mt-3 px-2'>
