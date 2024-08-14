045b71b34684716c4c0e81a9ef3045f179614fae
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-14 14:30:55 +0530

    updated sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -36,8 +36,6 @@ const Sidebar = () => {
 
 
         <div className='flex items-center gap-4 mt-3 px-2'>
-            
-            
         </div>
         <div className='flex items-center gap-4 mt-3 px-2'>
             <BiLike  size={20} />
