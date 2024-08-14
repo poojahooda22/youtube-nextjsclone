1db6389079d4369d30791aa8db09b68fa18662c2
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-14 13:44:04 +0530

    added remove

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -63,9 +63,7 @@ const Sidebar = () => {
 
         
         <div className='flex items-center  justify-between  gap-4 mt-3 px-2'>
-            <div className='flex items-center gap-4'>
-                
-            </div>
+            
             <HiOutlineStatusOnline size={20} color='red' />
         </div>
         <div className='flex items-center justify-between gap-4 mt-3 px-2'>
