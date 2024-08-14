8a56e1111a90f49d557c3a093962d952f8d83afc
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-14 13:35:58 +0530

    updated subscribe

--- src/components/Subscribe.tsx
+++ src/components/Subscribe.tsx
@@ -3,7 +3,27 @@ import React from 'react'
 const Subscribe = () => {
   return (
     <div>
-      
+      <div className='flex items-center justify-between gap-4 mt-3 px-2'>
+            <div className='flex items-center gap-4'>
+                <img src="/ndtv.jpg" className='w-[2.4vw] h-[2.4vw] rounded-full' />
+                <p className='text-[1.3vw]'>NDTV India</p>
+            </div>        
+            <HiOutlineStatusOnline size={20} color='red' />
+        </div>
+        <div className='flex items-center  justify-between  gap-4 mt-3 px-2'>
+            <div className='flex items-center gap-4'>
+                <img src="/nbc.jpg" className='w-[2.4vw] h-[2.4vw] rounded-full' />
+                <p className='text-[1.3vw]'>NBC News</p>
+            </div>
+            <HiOutlineStatusOnline size={20} color='red' />
+        </div>
+        <div className='flex items-center justify-between gap-4 mt-3 px-2'>
+            <div className='flex items-center gap-4'>
+                <img src="/express.jpg" className='w-[2.4vw] h-[2.4vw] rounded-full' />
+                <p className='text-[1.3vw]'>Indian Expr..</p>
+            </div>
+            <HiOutlineStatusOnline size={20} color='red' />
+        </div>
     </div>
   )
 }
