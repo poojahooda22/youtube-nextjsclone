5b9a2ae37e368842909fb593199fe47beafb5fb4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-14 13:54:42 +0530

    updated sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -36,7 +36,7 @@ const Sidebar = () => {
 
 
         <div className='flex items-center gap-4 mt-3 px-2'>
-            <MdPlaylistPlay size={20} />
+           
             <p className='text-[1.3vw]'>Playlists</p>
         </div>
         <div className='flex items-center gap-4 mt-3 px-2'>
