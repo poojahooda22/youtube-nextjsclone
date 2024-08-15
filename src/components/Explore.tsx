10a5d73286575aa39834c50b54360e5973aae481
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-15 15:33:53 +0530

    updated music

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -16,6 +16,11 @@ const Explore = () => {
             <MdOutlineShoppingBag size={20} />
             <h4 className='text-[1.3vw]'>Shopping</h4>
         </div>
+
+        <div className='mt-[2vw] flex items-center gap-2 px-2'>
+            <MdOutlineShoppingBag size={20} />
+            <h4 className='text-[1.3vw]'>Music</h4>
+        </div>
     </div>
   )
 }
