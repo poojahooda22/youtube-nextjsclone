7079b22687fbfe4691338533907896b97d7ca113
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-15 11:40:34 +0530

    updated sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -42,6 +42,9 @@ const Sidebar = () => {
         <h4 className='text-[1.5vw] mt-3 px-2'>Subscriptions</h4>
         <Subscribe/>
 
+        <div className='w-full h-[1px] bg-zinc-700 mt-4 px-2'></div>
+        
+
     </div>
   )
 }
