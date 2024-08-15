fca34beb736a1c86f36e460f33646421cde63329
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-15 15:33:30 +0530

    added

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -9,11 +9,12 @@ const Explore = () => {
     <div>
         <div className='mt-[2vw] flex items-center gap-2 px-2'>  
             <LiaFireSolid size={20} />
-            <h4 className='text-[1.6vw]'>Trending</h4>
+            <h4 className='text-[1.3vw]'>Trending</h4>
         </div>
 
         <div className='mt-[2vw] flex items-center gap-2 px-2'>
             <MdOutlineShoppingBag size={20} />
+            <h4 className='text-[1.3vw]'>Shopping</h4>
         </div>
     </div>
   )
