ce5a958ca14feff5d975c13bc0cd70f634222840
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-15 12:07:53 +0530

    updated icon with div

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Explore = () => {
   return (
     <div>
-      
+      <div className='mt-[2vw] flex items-center gap-2 px-2'>
+            <h4 className='text-[1.6vw]'>You</h4>
+            <IoIosArrowForward size={14} />
+        </div>
     </div>
   )
 }
