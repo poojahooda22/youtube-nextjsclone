382883c85f72f10dd15702ddcb93890fd8ae5cf9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-16 22:20:32 +0530

    added

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -56,7 +56,7 @@ const Explore = () => {
         </div>
 
         <div className='mt-[2vw] flex items-center gap-4 px-2'>
-            <MdOutlinePodcasts size={20} />
+            <MdOutlinePodcasts size={20}/>
             <h4 className='text-[1.3vw]'>Podcasts</h4>
         </div>
     </div>
