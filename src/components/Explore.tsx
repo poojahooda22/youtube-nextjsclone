dad5bc2f95f9e321ca9023e71ff6fb633fdd6074
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-16 08:00:12 +0530

    imported explore

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -56,7 +56,7 @@ const Explore = () => {
         </div>
 
         <div className='mt-[2vw] flex items-center gap-4 px-2'>
-            <GoTrophy size={20} />
+            <MdOutlinePodcasts size={20} />
             <h4 className='text-[1.3vw]'>Sports</h4>
         </div>
     </div>
