ca486995e1c8c045b7031c5dca638fbef8f1c24f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-16 21:05:27 +0530

    updated youtube

--- src/components/YouTube.tsx
+++ src/components/YouTube.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 const YouTube = () => {
   return (
     <div>
-      
+        <div className='mt-[2vw] flex items-center gap-4 px-2'>
+            <BiNews  size={20} />
+            <h4 className='text-[1.3vw]'>News</h4>
+        </div>
     </div>
   )
 }
