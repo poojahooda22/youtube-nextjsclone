2692cbe6996d5bda7ae1b68536290823bbee5063
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-16 22:08:40 +0530

    updated youtube

--- src/components/YouTube.tsx
+++ src/components/YouTube.tsx
@@ -13,7 +13,7 @@ const YouTube = () => {
 
         <div className='mt-[2vw] flex items-center gap-4 px-2'>
             <SiYoutubestudio  size={20} />
-            <h4 className='text-[1.3vw]'>Youtube</h4>
+            <h4 className='text-[1.3vw]'>Youtube studio</h4>
         </div>
 
         <div className='mt-[2vw] flex items-center gap-4 px-2'>
