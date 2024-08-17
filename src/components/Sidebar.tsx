0614cde16fbf9389c31096e76c32f8ada900aee9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-17 08:10:03 +0530

    updated sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -20,7 +20,8 @@ const Sidebar = () => {
     <div className='absolute top-0 w-[22vw] h-full bg-zinc-900 py-[1.5vw] px-[1.5vw]'>
         <div className='flex items-start'>
             <IoMenuSharp 
-                size={28} color='#999999' className='hidden sm:block' />  
+                size={28} 
+                color='#999999' className='hidden sm:block' />  
             <img src="/ytlogo.svg" alt="logo" className='w-28 h-8' />  
         </div>
         <div 
