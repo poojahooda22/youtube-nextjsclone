883636b8c5940e66549933d111daf5565e13328f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-17 08:15:10 +0530

    updated sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -32,7 +32,8 @@ const Sidebar = () => {
         >
             <GoHomeFill size={20} />
             <p className='text-[1.3vw]'>
-                Home</p>
+                Home
+                </p>
         </div>
         <div className='flex items-center gap-4 mt-3 px-2'>
             <SiYoutubeshorts size={20} />
