af675f0e742505a3d625dfc5b47365de74fb80d9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-17 08:13:33 +0530

    updated sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -23,7 +23,7 @@ const Sidebar = () => {
                 size={28} 
                 color='#999999' 
                 className='hidden sm:block' 
-                />  
+            />  
             <img src="/ytlogo.svg" alt="logo" className='w-28 h-8' />  
         </div>
         <div 
