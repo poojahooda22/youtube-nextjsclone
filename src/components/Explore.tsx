bf890e721ab22408d96e03b8966682ca68eef50d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-20 21:22:10 +0530

    added

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -42,7 +42,8 @@ const Explore = () => {
 
         <div className='mt-[2vw] flex items-center gap-4 px-2'>
             <PiFilmSlate 
-            size={20} />
+                size={20} 
+                />
             <h4 className='text-[1.3vw]'>Films</h4>
         </div>
 
