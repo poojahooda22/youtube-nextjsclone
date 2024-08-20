c8452eafa90427848d5e2faf87763a5a00f22717
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-20 21:18:44 +0530

    updated explore

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -17,7 +17,8 @@ const Explore = () => {
             <LiaFireSolid 
                 size={20} 
             />
-            <h4 className='text-[1.3vw]'>Trending</h4>
+            <h4 className='text-[1.3vw]'>
+                Trending</h4>
         </div>
 
         <div className='mt-[2vw] flex items-center gap-4 px-2'>
