f8a9e36da51d94a95580ad95c520eea3f9e5d031
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-20 20:10:57 +0530

    added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -30,7 +30,8 @@ const Sidebar = () => {
             <img 
                 src="/ytlogo.svg" 
                 alt="logo" 
-                className='w-28 h-8' />  
+                className='w-28 h-8' 
+                />  
         </div>
         <div 
             className='flex items-center gap-4 mt-6 
