26b7acaafabdb43a9bb7347a5a1cdd5fdfe2bb66
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-20 21:20:53 +0530

    added

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -28,7 +28,7 @@ const Explore = () => {
             />
             <h4 className='text-[1.3vw]'>
                 Shopping
-                </h4>
+            </h4>
         </div>
 
         <div className='mt-[2vw] flex items-center gap-4 px-2'>
