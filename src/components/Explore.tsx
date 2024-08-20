d25b17008e7cbc212cdb1733e2f97adca35be5b1
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-20 21:20:09 +0530

    added

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -24,7 +24,7 @@ const Explore = () => {
 
         <div className='mt-[2vw] flex items-center gap-4 px-2'>
             <MdOutlineShoppingBag 
-            size={20} />
+                size={20} />
             <h4 className='text-[1.3vw]'>Shopping</h4>
         </div>
 
