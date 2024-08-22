5c21fae08170e599750330792a614c22ddf6cfd4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-22 22:10:29 +0530

    updated

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -78,7 +78,8 @@ const Explore = () => {
 
         <div className='mt-[2vw] flex items-center gap-4 px-2'>
             <GoTrophy 
-            size={20} />
+                size={20} 
+            />
             <h4 className='text-[1.3vw]'>Sports</h4>
         </div>
 
