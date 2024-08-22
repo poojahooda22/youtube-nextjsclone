791b7dfe111e9d55df17f03de4d54ef7111754cf
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-22 22:07:12 +0530

    updated explore

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -63,7 +63,8 @@ const Explore = () => {
                 size={20} 
             />
             <h4 className='text-[1.3vw]'>
-                Gaming</h4>
+                Gaming
+                </h4>
         </div>
 
         <div className='mt-[2vw] flex items-center gap-4 px-2'>
