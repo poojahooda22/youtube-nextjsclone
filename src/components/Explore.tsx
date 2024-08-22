ca777b02107368b7bb1de302c430115ca98a8a04
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-22 22:08:48 +0530

    added

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -72,7 +72,8 @@ const Explore = () => {
                 size={20} 
             />
             <h4 className='text-[1.3vw]'>
-                News</h4>
+                News
+                </h4>
         </div>
 
         <div className='mt-[2vw] flex items-center gap-4 px-2'>
