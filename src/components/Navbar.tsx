1762e62fd878decf61bdfdace2e26e91e2c8f95a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-24 21:44:57 +0530

    added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         <IoMenuSharp size={30} 
           color='#999999' 
           className='hidden sm:block'
-          onClick={() => console.log('menu')}
+          onClick={() => console.log('men')}
         />  
         <img 
           src="/ytlogo.svg" 
