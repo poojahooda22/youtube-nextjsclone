9594dcc8d7ed1f0f49bc60df0c731fd3bb29677c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-30 12:19:57 +0530

    added

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -12,7 +12,8 @@ const Searchbar = () => {
                     className="block p-2.5 w-full z-20 text-sm 
                     text-gray-900 bg-gray-50 
                     rounded-full border-s-gray-50 border-s-2 
-                    border border-gray-300 focus:ring-blue-500
+                    border border-gray-300 
+                    focus:ring-blue-500
                     focus:border-blue-500 dark:bg-gray-900 
                     dark:border-s-gray-700  dark:border-gray-600 
                     dark:placeholder-gray-400 
