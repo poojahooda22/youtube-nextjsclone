33557ac2020b0aa6265c61b6d9cd5fa1fbe9a0a2
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-31 21:48:27 +0530

    updated searchbar

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -11,7 +11,7 @@ const Searchbar = () => {
                     id="search-dropdown" 
                     className="block p-2.5 w-full  z-20 text-sm 
                     text-gray-900 bg-gray-50 rounded-full border-s-gray-50 
-                    border-s-2 border border-gray-300 
+                    border-s-2 border border-gray-300
                     focus:ring-blue-500 focus:border-blue-500 
                     dark:bg-gray-900 dark:border-s-gray-700 dark:border-gray-600 
                     dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" 
