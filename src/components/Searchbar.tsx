c4fecd34f4b45f559e9dcc5d36f94fd8da9dc052
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-08-31 11:08:40 +0530

    updated searchbar

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -26,7 +26,8 @@ const Searchbar = () => {
                 />
                 <button 
                     type="submit" 
-                    className="absolute top-0 end-0 p-3.5 text-sm 
+                    className="absolute top-0 end-0 
+                    p-3.5 text-sm 
                     font-medium h-full text-white 
                     rounded-e-full focus:ring-4 
                     focus:outline-none 
