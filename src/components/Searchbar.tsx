b7dbcef1fe82e048f7d9cd5f5fa5fe3089d9010d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-09-01 10:01:20 +0530

    added

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -22,7 +22,7 @@ const Searchbar = () => {
                     className="absolute top-0 end-0 p-3.5 text-sm
                     font-medium h-full text-white rounded-e-full focus:ring-4 
                     focus:outline-none 
-                 focus:ring-blue-300"
+             focus:ring-blue-300"
                 >
                     <svg 
                         className="w-4 h-4" 
