2ddbc1e31952322b1a1236197c266bdafe7da62d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-12-07 15:02:39 +0530

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,28 @@ import Card from "@/components/Card";
 import FlashButton from "@/components/flash/FlashButton";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
+import Sidebar2 from "@/components/Sidebar2";
+import { SidebarProvider } from "@/components/SidebarContext";
+import Testing from "@/components/Testing";
+import TextAnimation from "@/components/text/TextAnimation";
+import Trigger from "@/components/Trigger";
 import { VideoGrid } from "@/components/VideoGrid";
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <SidebarProvider>
     <main className="py-[1vw]">
-      <Navbar />
-      {/* <Sidebar/> */}
-      <VideoGrid />
-      {/* <Button /> */}
+      {/* <Navbar />
+      <Sidebar/>
+      <Trigger /> */}
+      <Testing/>
+      <Sidebar2 />
+      {/* <VideoGrid />
+      <Button />
       <FlashButton />
-
+      <TextAnimation /> */}
     </main>
+    </SidebarProvider>
   );
 }
